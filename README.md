6ec61c88b6b4f70c9600eaef4f0f3bbf30d29289
Epaulais Rémi <epaulais.remi@gmail.com>
2024-04-29 17:49:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# kiovoapp.com
+
+This is my personal website where I'll publish all of my projects, from learning projects to personal ones!
---
+++ main.js
@@ -0,0 +1,16 @@
+let count = 0;
+let value = document.getElementById("value");
+document
+    .getElementsByTagName("button")[0]
+    .addEventListener("click", function () {
+        value.classList.remove("animation-trigger");
+        setTimeout(function () {
+            value.classList.add("animation-trigger");
+        }, 0);
+        count++;
+        value.innerText = `You've pressed the button ${count} times!`;
+    });
+value.addEventListener("animationend", function () {
+    value.classList.remove("animation-trigger");
+    isAnimating = false;
+});
